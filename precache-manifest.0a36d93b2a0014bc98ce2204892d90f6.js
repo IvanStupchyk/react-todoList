158b81c6933a9c2727169a5001b64ce76ab45e6e
IvanStupchuk <hardtrynew@rambler.ru>
2021-04-13 16:21:26 +0300

    Updates

---
+++ precache-manifest.0a36d93b2a0014bc98ce2204892d90f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de6fb271b1a070a44136a45ca893990",
+    "url": "/react-todoList/index.html"
+  },
+  {
+    "revision": "e930e49bc6022a7b4ccf",
+    "url": "/react-todoList/static/css/main.78668413.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/react-todoList/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todoList/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e930e49bc6022a7b4ccf",
+    "url": "/react-todoList/static/js/main.08bcf813.chunk.js"
+  },
+  {
+    "revision": "4acac388181472ca133a",
+    "url": "/react-todoList/static/js/runtime-main.e019f289.js"
+  }
+]);
